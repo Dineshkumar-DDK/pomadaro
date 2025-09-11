33158d0ece2d1d502c5d3a35437ef7ac8960255e
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2025-09-11 07:38:50 +0530

    completed the pomodaro

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const workTime = 25*60;
+const workTime =25*60; // 25 minutes in seconds
+const breakTime= 5*60;
 let timeLeft = workTime;
 let timer = null;
+let isWorkTime = true;
+let count = 0
 
 const timerArea = document.querySelector('#timer')
 const startButton = document.querySelector('#startBtn')
 const pauseButton = document.querySelector('#pauseBtn')
 const resetButton = document.querySelector('#resetBtn')
+const statusArea = document.querySelector('#status');
+const cyclesArea = document.querySelector('#cycles');
 
 
 function startTimer(){
@@ -16,8 +21,8 @@ function startTimer(){
         }else{
             clearInterval(timer);
             timer=null
+            toggleWorkBreak()
             updateUI();
-            timeLeft=workTime;
             startTimer()
         }
     },1000)
@@ -29,16 +34,35 @@ const updateUI = () =>{
     timerArea.textContent = `${minutes}:${seconds}`;
 }
 
+function stopTimer(){
+clearInterval(timer);
+isWorkTime = true;
+statusArea.textContent = "Focus Time";
+timer=null;
+}
 
 function pauseTimer(){
-    clearInterval(timer);
-    timer=null
+    stopTimer();
 }
 
 function resetTimer(){
-    pauseTimer();
+    stopTimer();
     timeLeft = workTime;
+    count=0;
+    cyclesArea.textContent = count;
     updateUI();
+}
+
+function toggleWorkBreak(){
+    updateCycles();
+    isWorkTime=!isWorkTime;
+    timeLeft = isWorkTime?workTime:breakTime;
+    statusArea.textContent = isWorkTime?"Work Time":"Break Time";
+}
+
+function updateCycles(){
+    isWorkTime && count++;
+    cyclesArea.textContent = count;
 }
 
 
